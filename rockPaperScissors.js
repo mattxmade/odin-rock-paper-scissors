491df989d546f474a450b3dec7dcb98d87539075
Matt <97047080+mattxmade@users.noreply.github.com>
2022-01-27 18:14:10 +0000

    Completed playerSelection() + input error handler functions

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -34,11 +34,57 @@
 // replay game if user wants to play again
 // exit if not + thanks for playing message
 
+//---------------------------------------------------------------------------------
+
+// 1)
 function computerPlay() {
+  // moves stored inside array
   const moveSelection = ['rock', 'paper', 'scissors'];
 
+  // Math methods used to return random number 0-2 based on index position of array
   return moveSelection[Math.floor(Math.random() * moveSelection.length)];
 }
 
 // debugging
 // console.log(computerPlay());
+
+// 2) Player
+function playerSelection() {
+  let playerMove = prompt('Rock Paper or Scissors?');
+
+  // user cancelled game
+  if (playerMove === null) {
+    confirm('Do you want to continue playing?') ? playerSelection() : alert('Thanks for playing!');
+  }
+
+  // covert input string to lower case
+  playerMove = playerMove.toLowerCase();
+
+  switch(playerMove) {
+    case 'rock':
+      break;
+
+    case 'paper':
+      break;
+
+    case 'scissors':
+      break;
+
+    default:
+      playerMove = invalidMove(playerMove);
+      alert(playerMove);
+      break;
+  }
+  // return to start if playerMove is invalid (scissors is 8 characters long - anything longer is treated as an error)
+  return playerMove.length > 8 ? playerSelection() : playerMove;
+}
+
+// debugging
+console.log(playerSelection());
+
+// 3) Input validator functions
+function invalidMove(playerMove) {
+  return playerMove === '' 
+  ? playerMove = `You didn't enter a move...` 
+  : playerMove = `Invalid move! The input of ${playerMove} is not recognised!`;
+}
